3e91cdfead462100b94a4ebbda61bc4bd3997c4e
Carlos Valencia <valenciacarlos278@gmail.com>
2022-12-21 13:49:13 -0500

    Added 2nd signal

--- exampleProyect.py
+++ exampleProyect.py
@@ -2,12 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-Fs = 8000
-f = 5
-sample = 8000
+Fs = 44100
+f = 400
+f2 = 800
+sample = 300
 x = np.arange(sample)
-y = np.sin(2 * np.pi * f * x / Fs)
-plt.plot(x, y)
+y1 = np.sin(2 * np.pi * f * x / Fs)
+y2 = np.sin(2*np.pi*f2*x/Fs)
+plt.plot(x, y1,'red')
+plt.plot(x,y2,'blue')
 plt.xlabel('sample(n)')
 plt.ylabel('voltage(V)')
 plt.show()
+
