010aa0d871f32a5975992a155b73ba81d6af0a58
Carlos Valencia <valenciacarlos278@gmail.com>
2022-12-21 14:08:47 -0500

    Error en la señal

--- exampleProyect.py
+++ exampleProyect.py
@@ -2,15 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-Fs = 44100
+Fs = 8000
 f = 400
 f2 = 800
 sample = 300
-x = np.arange(sample)
-y1 = np.sin(2 * np.pi * f * x / Fs)
-y2 = np.sin(2*np.pi*f2*x/Fs)
-plt.plot(x, y1,'red')
-plt.plot(x,y2,'blue')
+t = np.arange(0,60,1.0/Fs)
+y1 = np.sin(2 * np.pi * f * t/ Fs)
+y2 = np.sin(2*np.pi*f2*t/Fs)
+yerr = 0.1 * np.random.normal(size=len(t)) 
+plt.plot(t, y1 + yerr,'red')
+plt.plot(t,y2,'blue')
 plt.xlabel('sample(n)')
 plt.ylabel('voltage(V)')
 plt.show()
