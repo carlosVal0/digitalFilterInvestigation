87db2c984c53efbd2cd2ea14f8baf1db28922d64
Carlos Valencia <valenciacarlos278@gmail.com>
2022-12-21 15:12:27 -0500

    Resonator filter

--- exampleProyect.py
+++ exampleProyect.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import digitalFilters
 
 Fs = 44100
 f = 400
@@ -9,9 +9,12 @@ sample = 300
 x = np.arange(sample)
 y1 = np.sin(2 * np.pi * f * x / Fs)
 y2 = np.sin(2*np.pi*f2*x/Fs)
-plt.plot(x, y1,'red')
-plt.plot(x,y2,'blue')
+y3 = y1 + y2
+y4 = digitalFilters.resonatorFilter(0.9,Fs,f,x,y3)
+print(y4)
+plt.figure()
+plt.plot(x, y3,'red')
+plt.plot(x,y4,'blue')
 plt.xlabel('sample(n)')
 plt.ylabel('voltage(V)')
 plt.show()
-
