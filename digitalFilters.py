6509d8b057dfaa6df1b362a4eb3c917ba03c36e5
Carlos Valencia <valenciacarlos278@gmail.com>
2022-12-28 17:36:04 -0500

    digitalFilters library partially cleaned

--- digitalFilters.py
+++ digitalFilters.py
@@ -7,20 +7,23 @@ def resonator_filter(input_signal, fs, time, f0):
     filtered_signal = signal.filtfilt(b, a, input_signal)
     return filtered_signal
 
-t = 0.5
-fs = 8000
-f0 = 1200
-t_array = np.arange(t*fs)
 
-x = np.sin(2*np.pi*f0/fs*t_array)
-x1 = np.sin(2*np.pi*60/fs*t_array)
-y = resonator_filter(x1+x,fs,t_array,f0)
+if __name__ == "__main__":
+        
+    t = 0.5
+    fs = 8000
+    f0 = 1200
+    t_array = np.arange(t*fs)
 
-plt.figure()
-plt.plot(t_array,x1,'red')
-plt.show()
+    x = np.sin(2*np.pi*f0/fs*t_array)
+    x1 = np.sin(2*np.pi*60/fs*t_array)
+    y = resonator_filter(x1+x,fs,t_array,f0)
 
-plt.figure()
-plt.plot(t_array,x1+x,'red')
-plt.plot(t_array,y,'blue')
-plt.show()
+    plt.figure()
+    plt.plot(t_array,x1,'red')
+    plt.show()
+
+    plt.figure()
+    plt.plot(t_array,x1+x,'red')
+    plt.plot(t_array,y,'blue')
+    plt.show()
